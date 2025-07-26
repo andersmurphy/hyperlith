ccb3db1f7b89cd52ad7ff0ef27809991f4ff6c9d
andersmurphy <andersmurphy@gmail.com>
2025-07-26 15:45:05 +0100

    Checkboxes: add QR code to share link

---
+++ examples/billion_checkboxes_blob/src/app/qrcode.clj
@@ -0,0 +1,16 @@
+(ns app.qrcode
+  (:require [hyperlith.core :as h])
+  (:import [io.nayuki.qrcodegen QrCode QrCode$Ecc]))
+
+(defn url->qrcode-svg [url]
+  (let [qr   (QrCode/encodeText url QrCode$Ecc/MEDIUM)
+        path (let [sb (StringBuilder.)]
+               (doseq [y (range 0 (.size qr))
+                       x (range 0 (.size qr))]
+                 (if (.getModule qr x y)
+                   (when (and (not= x 0) (not= y 0)) (.append sb " "))
+                   (.append sb (format "M%d,%dh1v1h-1z" x y))))
+               (.toString sb))]
+    (h/html
+      [:svg {:width "100%" :viewBox "0 0 29 29"}
+       [:path {:d path}]])))
