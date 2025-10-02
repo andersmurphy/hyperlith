0d19aea5d216fac059f8b6f68c8302acfed166b4
andersmurphy <andersmurphy@gmail.com>
2025-10-02 22:46:44 +0100

    Wait for litestream 5 full release

--- examples/litestream-setup.sh
+++ examples/litestream-setup.sh
@@ -1,3 +1,3 @@
-wget https://github.com/benbjohnson/litestream/releases/download/v0.5.0-test.2/litestream-v0.5.0-test.2-linux-amd64.deb
+wget https://github.com/benbjohnson/litestream/releases/download/v0.3.13/litestream-v0.3.13-linux-amd64.deb
 
-sudo dpkg -i litestream-v0.5.0-test.2-linux-amd64.deb
+sudo dpkg -i litestream-v0.3.13-linux-amd64.deb
