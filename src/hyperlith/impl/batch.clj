fa1d90a0d0f4baacba36dc7dcec23c4202e814b8
andersmurphy <andersmurphy@gmail.com>
2025-11-02 09:37:18 +0000

    Use atom for faster lockless batching

--- src/hyperlith/impl/batch.clj
+++ src/hyperlith/impl/batch.clj
@@ -1,6 +1,5 @@
 (ns hyperlith.impl.batch
-  (:require [clojure.core.async :as a]
-            [hyperlith.impl.util :as util]
+  (:require [hyperlith.impl.util :as util]
             [hyperlith.impl.error :as er]
             [hyperlith.impl.cpu-pool :as cp]))
 
@@ -9,31 +8,34 @@
   batch is run every X ms when not empty. Function must take a vector of items. Supports back pressure."
   [effect-fn & {:keys [run-every-ms]
                 :or   {run-every-ms 100}}]
-  (let [<in (a/chan 1000)] ;; potentially pass this channel in
-    (util/thread
-      (loop [<t    (a/timeout run-every-ms)
-             batch []]
-        (let [[v p] (a/alts!! [<t <in] :priority true)]
-          (cond
-            ;; Reset timer
-            (and (= p <t) (= (count batch) 0))
-            (recur (a/timeout run-every-ms) batch)
-
-            ;; Run batch
-            (= p <t)
-            (let [;; Timer is started before task to prevent task duration
-                  ;; from affecting interval (unless it exceeds interval).
-                  <new-t (a/timeout run-every-ms)]
-              (cp/on-cpu-pool ;; CPU work on real threads
-                (er/try-on-error (effect-fn batch)))
-              (recur <new-t []))
-
-            ;; Add to batch
-            (= p <in)
-            (recur <t (conj batch v))
-
-            ;; if upstream is close run final batch and stop
-            :else
-            (er/try-on-error (effect-fn (apply concat batch)))))))
+  (let [batch_ (atom [])]
+    (util/thread ;; this is v thread
+      (loop []
+        (if (= (count @batch_) 0)
+          ;; Reset timer
+          (do (Thread/sleep ^long run-every-ms)
+              (recur))
+          ;; Run batch
+          (let [;; Timer is started before task to prevent task duration
+                ;; from affecting interval (unless it exceeds interval).
+                start     (System/currentTimeMillis)
+                [batch _] (reset-vals! batch_ [])]
+            (cp/on-cpu-pool ;; CPU work on real threads
+              (er/try-on-error (effect-fn batch)))
+            (let [delta (- run-every-ms (- (System/currentTimeMillis) start))]
+              (when (> delta 0)
+                (Thread/sleep ^long delta)))
+            (recur)))))
     (fn [items]
-      (a/>!! <in items))))
+      (swap! batch_ conj items))))
+
+
+(comment
+  (def batch-v1
+    (batch! (fn [_] (Thread/sleep 10))
+      {:run-every-ms 100}))
+
+  (user/bench
+    (->> (mapv (fn [_] (future (batch-v1 1)))
+           (range 0 2000))
+         (run! (fn [x] @x)))))
