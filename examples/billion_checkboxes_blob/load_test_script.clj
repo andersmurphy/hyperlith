b563ea68692bff3a58c31fcd6d3a38fa3ca09b4e
andersmurphy <andersmurphy@gmail.com>
2026-06-19 12:05:47 +0100

    Improve profiling script

--- examples/billion_checkboxes_blob/load_test_script.clj
+++ examples/billion_checkboxes_blob/load_test_script.clj
@@ -9,20 +9,36 @@
   (http/client
     (assoc-in http/default-client-opts [:ssl-context :insecure] true)))
 
-;; Light update load roughly 1000req/s 
-(let [url    "https://localhost:3030/t_rqnpSL_NvK8EJhoBwkc6TNJ4VsLi1Fs"
-      cookie "__Host-sid=vtwl34jCOZDoGVIORWfiCBKg0U0"]
+(def headers
+  {"Cookie"          "__Host-sid=vtwl34jCOZDoGVIORWfiCBKg0U0"
+   "Accept-Encoding" "br"
+   "sec-fetch-site"  "same-origin"})
+
+(let [url "https://localhost:3030/?u="]
+  (dotimes [_ 100]
+    (Thread/startVirtualThread
+      (fn []
+        (try
+          (let [resp (http/get url
+                       {:client  client
+                        :headers headers
+                        :as      :stream})]
+            (with-open [in (:body resp)]
+              (while true
+                (.skip in Long/MAX_VALUE)
+                (Thread/sleep 10))))
+          (catch Exception _)
+          (println "Stream closed"))))))
+
+(let [url "https://localhost:3030/t_rqnpSL_NvK8EJhoBwkc6TNJ4VsLi1Fs"]
   (dotimes [_ 10000]
     (Thread/startVirtualThread
       (fn []
         (http/post url
           {:client  client
-           :headers {"Cookie"          cookie
-                     "Accept-Encoding" "br"
-                     "sec-fetch-site"  "same-origin"
-                     "Content-Type"    "application/json"}
+           :headers (assoc headers "Content-Type" "application/json")
            :body    (json/encode
                       {"tabid"    "7dc673ca"
                        "targetid" (str (rand-int 255))
                        "parentid" (str 0)})})))
-    (Thread/sleep 1)))
+    (Thread/sleep 50)))
