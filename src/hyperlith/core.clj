5e54433ab64a72b4793f57709b243346cb6d19bc
andersmurphy <andersmurphy@gmail.com>
2026-09-18 22:32:17 +0100

    Size lane buffer

--- src/hyperlith/core.clj
+++ src/hyperlith/core.clj
@@ -122,9 +122,9 @@
                :attr-name-cache   (HashMap.)
                ;; Goes back onto the heap when converted to byte array
                :attr-byte-scratch (ByteBuffer/allocate 16384)
-               :html-dst-buf      (ByteBuffer/allocate (* 20 16384))
+               :html-dst-buf      (ByteBuffer/allocate (* 32 16384))
                ;; zstd is in native lang
-               :zstd-src-buf      (ByteBuffer/allocateDirect (* 20 16384))})))))
+               :zstd-src-buf      (ByteBuffer/allocateDirect (* 32 16384))})))))
 
 (defn- submit-values-to-lanes! [lanes v]
   (let [lanes-count (count lanes)]
